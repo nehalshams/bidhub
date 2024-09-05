054cd46c8d7b8f09c7c159cf371364d5085b7361
Nehal Shams <nehalshams786@gmail.com>
2024-09-05 15:53:41 +0530

    update auction api

---
+++ server/src/models/auction.model.ts
@@ -0,0 +1,25 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IDomain extends Document {
+  name: string;
+  description: string;
+  startingPrice: number;
+  currentPrice?: number;
+  status: 'active' | 'closed';
+  auctionEndTime: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const AuctionSchema: Schema = new Schema({
+  domainName: { type: String, required: true },
+  description: { type: String },
+  startingPrice: { type: Number, required: true },
+  currentPrice: { type: Number },
+  status: { type: String, enum: ['active', 'closed'], default: 'active' },
+  auctionEndTime: { type: Date, required: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.model<IDomain>('Auction', AuctionSchema);
