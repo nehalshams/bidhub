dfaad0f7c9cea65fd2e78e6eb5b371cbe099aaac
Nehal <nehalshams786@gmail.com>
2024-09-18 21:25:37 +0530

    allow cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,20 +12,7 @@ dotenv.config();
 // Initialize Express app
 const app: Application = express();
 
-
-const allowedOrigins = ['http://localhost:3000', 'https://bidhub-five.vercel.app'];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 app.use(express.json());
 
