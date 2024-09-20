3b4c699ab8fe32ffb6067bfbc1ce05cb5d7ee4b4
Nehal <nehalshams786@gmail.com>
2024-09-20 16:41:07 +0530

    redeploy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,6 @@ import { ToastContainer } from "react-toastify";
 import { Box, useTheme } from "@mui/material";
 function App() {
   const theme = useTheme();
-  
   return (
     <Box sx={{ height: '100vh', backgroundColor: theme.palette.primary['light']}}>
       <Router/>
