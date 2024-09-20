e38e8198dbbffc93dd48120c79bec24bdbb129ae
Nehal <nehalshams786@gmail.com>
2024-09-20 15:50:52 +0530

    allow cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,5 @@
 import express, { Application } from 'express';
-import cors from 'cors';
+import cors, { CorsOptions } from 'cors';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.routes';
 import auctionRoutes from './routes/auction.routes';
@@ -12,7 +12,26 @@ dotenv.config();
 // Initialize Express app
 const app: Application = express();
 
-app.use(cors());
+// Define allowed origins
+const allowedOrigins = ['https://bidhub-five.vercel.app', 'http://localhost:3000'];
+
+// Define CORS options with proper types
+const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+    // Allow requests with no origin, like mobile apps or curl
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true); // Accept the request
+    } else {
+      callback(new Error('Not allowed by CORS')); // Reject the request
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true, // Allow credentials (cookies, authorization headers)
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
